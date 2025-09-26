b2ff6d0014fd7fcc7ec1910694f2c9e645189e4c
Retesh Kumar Sharma <rksharma7071@gmail.com>
2025-09-26 14:16:58 +0530

    version 0.0.33

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://blogsphare-api.onrender.com",
-        // target: "http://localhost:3000/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-=======
->>>>>>> ecbb0cf1069a99186a9f5464c8291902aec651f7
 });
